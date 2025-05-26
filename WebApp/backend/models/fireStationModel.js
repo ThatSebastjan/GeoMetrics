a6f09ff52acb732b20dc2886ed34d819e1f9c11a
Rocky-M7 <rok.geric7@gmail.com>
2025-05-26 11:48:53 +0200

    SCRUM-49 Added earthquake and fire station models
    Earthquakes and fire stations are represented as GeoJSON features with additional properties.
    Everything is indexed by location, so spatial queries can be performed.

---
+++ WebApp/backend/models/fireStationModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const { pointSchema } = require("./GeoJsonSchema.js");
+const Schema = mongoose.Schema;
+
+
+const fireStationSchema = new Schema({
+    type: {
+        type: String,
+        required: true
+    },
+
+    id: {
+        type: Number,
+        required: true,
+        unique: true,
+    },
+
+    geometry: pointSchema,
+
+    properties: {
+        location: String,
+        address: String,
+        city: String,
+        description: String,
+        telephoneNumber: String,
+    }
+});
+
+fireStationSchema.index({ geometry: "2dsphere" });
+
+
+
+module.exports = mongoose.model("fireStations", fireStationSchema);
